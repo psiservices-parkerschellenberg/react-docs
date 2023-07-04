6746c12a1f22635c15b3bffdcf32c7308839ced4
Parker Schellenberg <parker.schellenberg@psionline.com>
2023-07-04 10:33:27 -0500

    Add embeded variable

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import MyButton from './components/MyButton';
 
+const user = {
+    name: "Parker",
+    age: 27,
+    loggedIn: true
+};
+
 function App() {
   return (
     <div className="App">
-      <h1>Welcome!</h1>
+          <h1>Welcome {user.name}!</h1>
       <MyButton/>
     </div>
   );

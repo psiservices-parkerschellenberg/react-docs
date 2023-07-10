50214258446a5f9e4cc66eef870a7a85de074473
Parker Schellenberg <parker.schellenberg@psionline.com>
2023-07-10 16:30:44 -0500

    Event handling

--- src/components/Form1.js
+++ src/components/Form1.js
@@ -1,12 +1,17 @@
 import styles from './Form1.module.css';
 
 export default function Form1() {
+
+    const handleChange = (e) => {
+        console.log(e.target.value);
+    };
+
     return (
         <div className={styles.form} >
             <h2 className={styles.title}>Form 1</h2>
             <div className={styles.formGroup}>
                 <label htmlFor="name" className={styles.formLabel}>Name</label>
-                <input id="name" type="text" className={styles.formInput} />
+                <input id="name" type="text" className={styles.formInput} onChange={handleChange} />
             </div>
             <div className={styles.formGroup}>
                 <label htmlFor="phone" className={styles.formLabel}>Phone</label>
